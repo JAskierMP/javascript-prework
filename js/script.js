1484d78d78b528b39a34077bc269b9ae7f2e65db
Mateusz Pitulski <jaskiermp@gmail.com>
2022-01-24 10:28:31 +0100

    fixed player move :)

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
 
 function getMoveName(argMoveId){
  if(argMoveId == 1){
